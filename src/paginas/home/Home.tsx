e655eae0147fd31c5f036f26d8ce4c24b35bb094
Giselle Souza <reallovetour@gmail.com>
2023-05-11 21:08:21 -0300

    Projeto react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+      <div id="conteiner">
+        <img src="src\assets\images\bannerPernaLonga.png" alt="Imagem do personagem Perna Longa meditando"></img>
+        <h1>Bora meditar juntos?</h1>
+        <p>Respira fundo e solta o play! Pois a oração abaixo representa todos nos programadores...</p>
+        <iframe className="video" width="560" height="315" src="https://www.youtube.com/embed/Bv2sDkQitLY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+      </div>
+    </>
+  )
+}
+
+export default Home;
