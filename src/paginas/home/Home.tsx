56c0035876d95444aef883c53f23b4dce8903f46
Giselle Souza <reallovetour@gmail.com>
2023-05-15 09:49:42 -0300

    Caminho da imagem

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <>
       <div id="conteiner">
-        <img src="..\..\src\assets\images\bannerPernaLonga.png"></img>
+        <img src="https://i.imgur.com/7iMHjez.png"/>
         <h1>Bora meditar juntos?</h1>
         <p>Respira fundo e solta o play! Pois a oração abaixo representa todos nos programadores...</p>
         <iframe className="video" width="560" height="315" src="https://www.youtube.com/embed/Bv2sDkQitLY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
